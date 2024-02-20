908b3fca068bb961a3623dbb544939b45f2944e2
Tomas Frongillo <38133977+tomifrongi@users.noreply.github.com>
2024-02-20 12:59:20 -0300

    first version of grid model

--- star-match-game/src/App.tsx
+++ star-match-game/src/App.tsx
@@ -1,23 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [numbers, setNumbers] = useState(
+    Array.from({ length: 9 }, (_, i) => i + 1)
+  );
+
+  const handleClick = (number: number) => {
+    console.log(`Clicked ${number}`);
+  };
+
+  let starsOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const getRandomNumber = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const randomIndex = getRandomNumber(0, starsOptions.length - 1);
+  const selectedStars = starsOptions[randomIndex];
+
+  const starsToShow = Array.from({ length: selectedStars }, (_, i) => i + 1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={2}>
+                <p>
+                  Seleccione una combinacion de numeros que logre la cantidad de
+                  estrellas
+                </p>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div className="gridStar">
+                  {starsToShow.map((star) => (
+                    <span key={star} role="img" aria-label="star">
+                      ⭐
+                    </span>
+                  ))}
+                </div>
+              </td>
+              <td>
+                <div className="gridNumber">
+                  {numbers.map((number) => (
+                    <button
+                      key={number}
+                      className="numberButton"
+                      onClick={() => handleClick(number)}
+                    >
+                      {number}
+                    </button>
+                  ))}
+                </div>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <td colSpan={2}>
+              <p>tiempo restante para resolver: {Math.random()}</p>
+            </td>
+          </tfoot>
+        </table>
       </header>
     </div>
   );
